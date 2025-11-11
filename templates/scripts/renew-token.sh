fec571344f4dda647d6610caf48eda66892234bc
Isaac Whitfield <iw@whitfin.io>
2025-11-11 12:39:23 -0500

    Update to use new Amazon APIs for tokens

--- templates/scripts/renew-token.sh
+++ templates/scripts/renew-token.sh
@@ -4,7 +4,7 @@ echo "Renewing AWS ECR token..."
 
 # retry till new get new token
 while true; do
-  TOKEN=$(aws ecr get-login --no-include-email --region $AWS_REGION | awk '{ print $6 }')
+  TOKEN=$(aws ecr get-authorization-token --region $AWS_REGION --query 'authorizationData[*].authorizationToken' --output text)
   if [ ! -z "${ESC}{TOKEN}" ]; then
     break
   fi
@@ -13,11 +13,8 @@ while true; do
 done
 echo "Successfully fetched new token"
 
-# convert the token into the base64 format we need to supply to nginx
-TOKEN=$(echo AWS:${ESC}{TOKEN} | base64 | tr -d "[:space:]")
-
 # create the token file with the new token header
-echo -n "Basic ${ESC}{TOKEN}" > $NGINX_CONFIG_DIR/.ecr-token
+echo -n "$TOKEN" > $NGINX_CONFIG_DIR/.ecr-token
 
 # reload the nginx service if it was running
 if test -f $NGINX_DIR/logs/nginx.pid; then
